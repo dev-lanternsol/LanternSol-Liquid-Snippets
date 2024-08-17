a9a68e1dd254b624bb2603d389b4dc072de47b53
dev-lanternsol <dev@lanternsol.com>
2024-08-17 01:56:12 -0500

    Liquid snippets added + bootstrap + github markup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
+import { useState } from "react";
+import { liquidSnippets } from "./liquid_snippets";
 import logo from './logo.svg';
+import Accordion from "./accordion";
 import './App.css';
 
 function App() {
+  const [query, setQuery] = useState("");
+  const keys = ["title", "description"]
+
+  const search = (data) => {
+    return data.filter(
+      (item) =>
+        keys.some(key=>item[key]?.toLowerCase().includes(query.toLowerCase()))
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar bg-info-subtle">
+        <div className="container-fluid py-3 px-5">
+          <a className="navbar-brand" href="https://www.lanternsol.com/" target="_blank" rel="noopener noreferrer">
+            <img className="img-fluid" src={logo} alt="lanternsol" width="200" height="40"/>
+          </a>
+          <a href="https://shopify.github.io/liquid/" className="btn btn-outline-success">Liquid documentation</a>
+        </div>
+      </nav>
+      <div className="container">
+        <div className="card mt-3">
+          <p className="text-start p-5 pb-0">Perform a search between all the liquid snippets.</p> 
+          <div className="input-group mx-auto p-5 pt-0">
+            <span className="input-group-text">Topic</span> 
+            <input 
+              type="text"
+              placeholder="Search..."
+              className="form-control"
+              onChange={(e) => setQuery(e.target.value)} 
+            />
+          </div>
+        </div>
+      </div>
+      <div className="container p-3">
+        <Accordion data={ search(liquidSnippets) }/> 
+      </div>
     </div>
   );
 }
---
+++ src/accordion.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import 'github-markdown-css/github-markdown.css';
+
+const Accordion = ({ data }) => {
+    return (
+        <div className="accordion" id="accordionExample">
+            {data.map((item, index) => {
+                const collapseId = `collapse-${index}`;
+                const headerId = `heading-${index}`;
+
+                return (
+                    <div className="accordion-item" key={index}>
+                        <h2 className="accordion-header" id={headerId}>
+                            <button
+                                className="accordion-button collapsed"
+                                type="button"
+                                data-bs-toggle="collapse"
+                                data-bs-target={`#${collapseId}`}
+                                aria-expanded="false"
+                                aria-controls={collapseId}
+                            >
+                                {item.title || "No title available"}
+                            </button>
+                        </h2>
+                        <div
+                            id={collapseId}
+                            className="accordion-collapse collapse"
+                            aria-labelledby={headerId}
+                            data-bs-parent="#accordionExample"
+                        >
+                            <div className="accordion-body markdown-body text-start m-0 m-md-5">
+                                <ReactMarkdown>
+                                    {item.description || "No description available"}
+                                </ReactMarkdown>
+                            </div>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
+}
+
+export default Accordion;
